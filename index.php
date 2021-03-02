3c3da2a49fced607d9d2079885f208634f973f63
ashikur37 <piash3700@gmail.com>
2021-03-02 21:27:09 +0600

    final_2-3-2021

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <?php
-    // for($i=0;$i<=100;$i++){
-    //     if($i%2==0){
-    //         echo "<b style='color:red;'>$i</b><br>";
-    //     }
-    //     else{
-    //         echo "<b style='color:green;'>$i</b><br>";
-    //     }
-    // }
-    // <?php echo "output"; 
-    // <?= "output"
+$x=90;
+// if($x>80)
+// echo "A+";
+// else if($x>70)
+// echo "A";
+// else 
+// echo "F";
+switch($x){
+    case $x>80:
+        
+    case $x>70:
+        echo "A";
+       
+    default:
+        echo "F";
+        break;
+}
 
-            // $x=51;
-            // echo $x%2==0?"Even":"Odd";
-            //concat 
-            //["red","green"][]
-for($i=0;$i<=100;$i++){
-        
-            ?>
-            <!-- <b style="color:<?=$i%2==0?'red':'green'?>"><?=$i?></b><br> -->
-            <b style="color:<?=["red","green"][$i%2]?>"><?=$i?></b><br>
-            <?php
-        
-    }
+
 ?>
 
     <!-- this is a html comment -->
