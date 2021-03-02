3a6e5247673727c872e1044a41c65ec876ee3501
ashikur37 <piash3700@gmail.com>
2021-03-02 20:36:21 +0600

    use_if_else_with_loop

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
+    // for($i=0;$i<=100;$i++){
+    //     if($i%2==0){
+    //         echo "<b style='color:red;'>$i</b><br>";
+    //     }
+    //     else{
+    //         echo "<b style='color:green;'>$i</b><br>";
+    //     }
+    // }
+    // <?php echo "output"; 
+    // <?= "output"
+    
     for($i=0;$i<=100;$i++){
-        echo "<b style='color:red;'>$i</b><br>";
+       
+            ?>
+            <b style='color:<?php 
+                if($i%2==0)
+                echo "red";
+                else
+                echo "green";
+            ?>;'><?=$i?></b><br>
+            
+<?php
+        
     }
+
 ?>
 
     <!-- this is a html comment -->
