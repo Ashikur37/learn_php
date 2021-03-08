0a8c41a1eff82ead258fc0601147634dfafda403
ashikur37 <piash3700@gmail.com>
2021-03-08 20:48:23 +0600

    prime_number_solution

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
 <?php
-$x=90;
-// if($x>80)
-// echo "A+";
-// else if($x>70)
-// echo "A";
-// else 
-// echo "F";
-switch($x){
-    case $x>80:
-        
-    case $x>70:
-        echo "A";
-       
-    default:
-        echo "F";
-        break;
+
+
+function isPrime($x){
+    
+    //check is prime or not
+    for($i=2;$i<$x;$i++){
+        if($x%$i==0){
+            return false;
+        }
+    }
+    //11
+
+// x i
+
+// 11 2  11 3 11 4 11 5 11 6 11 7 11 8 11 9 11 10
+
+//12 
+    return true;
+    //return true if prime
+
+    //return false not prime
 }
 
 
+for($i=1;$i<=1500;$i++){
+    if(isPrime($i)){
+        echo $i."<br>";
+    }
+}
 ?>
 
-    <!-- this is a html comment -->
+
 
 
 
