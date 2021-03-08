a0206a87aa3ce11c55fc329f3dd65b95944abe0b
ashikur37 <piash3700@gmail.com>
2021-03-08 21:02:27 +0600

    updated

--- index.php
+++ index.php
@@ -1,33 +1,10 @@
 <?php
 
+$cities=["dhaka","chittagong","khulna","barishal","rajshahi"];
 
-function isPrime($x){
-    
-    //check is prime or not
-    for($i=2;$i<$x;$i++){
-        if($x%$i==0){
-            return false;
-        }
+    for($i=0;$i<count($cities);$i++){
+            echo str_replace("k","<b style='color:red'>k</b>","<b style='color:green'>".$cities[$i]."</b><br>");
     }
-    //11
-
-// x i
-
-// 11 2  11 3 11 4 11 5 11 6 11 7 11 8 11 9 11 10
-
-//12 
-    return true;
-    //return true if prime
-
-    //return false not prime
-}
-
-
-for($i=1;$i<=1500;$i++){
-    if(isPrime($i)){
-        echo $i."<br>";
-    }
-}
 ?>
 
 
