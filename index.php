32d4782bb02558665ec450fcbe7b4cbc7edbfcec
ashikur37 <piash3700@gmail.com>
2021-03-08 21:04:35 +0600

    str_replace_dhaka_comilla

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $cities=["dhaka","chittagong","khulna","barishal","rajshahi"];
 
     for($i=0;$i<count($cities);$i++){
-            echo str_replace("k","<b style='color:red'>k</b>","<b style='color:green'>".$cities[$i]."</b><br>");
+            echo str_replace("dhaka","<b style='color:red'>comilla</b>","<b style='color:green'>".$cities[$i]."</b><br>");
     }
 ?>
 
