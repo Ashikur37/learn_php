982084c4a75b47a4c3a186e9fcfd5e0a05c2e796
ashikur37 <piash3700@gmail.com>
2021-03-25 21:24:38 +0600

    basic_form_validation

--- index.php
+++ index.php
@@ -5,16 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-        <form action="second.php" method="post">
-                <input type="text" name="username">
-                <input type="password" name="password">
-                gender <input name="gender" type="radio" value="male"> Male <input name="gender" type="radio" value="female"> Female
-                <select name="country" id="">
-                    <option value="BD">Bangladesh</option>
-                    <option value="AU">Australia</option>
 
-                </select>
+<body>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $valid=true;
+            if(strlen($_POST["username"])<6){
+                $valid=false;
+                $usernameError="Length of username must be 6";
+            }
+            if(strcmp($_POST["password"],$_POST["confirm_password"])!=0){
+                $valid=false;
+                $passwordError="Passwords doesn't match";
+            }
+            else if(strlen($_POST["password"])<6){
+                $valid=false;
+                $passwordError="Length of password must be 6";
+
+            }
+            if($valid){
+                header('location:second.php');
+            }
+        }
+    ?>
+        <form action="index.php" method="post">
+                    <input placeholder="Enter username" type="text" name="username">
+                    <?php
+                        if(isset($usernameError)){
+                            echo $usernameError;
+                        }
+                    ?>
+                    <br>
+                    <input placeholder="Enter password" type="password" name="password"><br>
+                    <input placeholder="Confirm password" type="password" name="confirm_password">
+                    <?php
+                        if(isset($passwordError)){
+                            echo $passwordError;
+                        }
+                    ?>
+                    <br>
+
                 <button>Submit</button>
         </form>
 </body>
--- second.php
+++ second.php
@@ -1,8 +1,6 @@
 <?php
-    echo $_POST["username"]."<br>";
-    echo $_POST["password"]."<br>";
-    echo $_POST["gender"]."<br>";
-    echo $_POST["country"]."<br>";
+    echo "Success";
+    
 
 
 
